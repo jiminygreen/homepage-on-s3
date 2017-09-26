96ef43184bf4a5cc4a068e4d71efe9f814fcf0a8
Greg Collins <gcollins@australianunity.com.au>
2017-09-26 22:43:16 +1000

    Added AU homepage as sample. Sync is being used to upload the directory.

--- deploy.bat
+++ deploy.bat
@@ -1,1 +1,1 @@
-aws s3api put-object --bucket homepage-static-website-s3bucket-v3smbnoesegz --key index.html --body ./source/index.html --acl public-read --content-type text/html --profile personal
+aws s3 sync --acl public-read --sse --delete ./source s3://homepage-static-website-s3bucket-v3smbnoesegz --profile personal
--- setup/deploy.bat
+++ setup/deploy.bat
@@ -1,1 +1,3 @@
-aws cloudformation deploy --stack-name homepage-static-website --template-file ./setup.yaml --profile personal
+aws cloudformation deploy --stack-name homepage-static-website --template-file ./setup.yaml --profile personal 
+
+aws s3api put-object --bucket homepage-static-website-s3bucket-v3smbnoesegz --key index.html --body ./source/index.html --acl public-read --content-type text/html --profile personal && aws s3api put-object --bucket homepage-static-website-s3bucket-v3smbnoesegz --key error.html --body ./source/error.html --acl public-read --content-type text/html --profile personal
